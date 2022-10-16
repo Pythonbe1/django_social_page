8f53ce61abb591e8429469586640c401e026efc9
Pythonbe1 <bekbol.sultankulov2@gmail.com>
2022-10-17 00:12:23 +0600

    added login page

--- core/views.py
+++ core/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from .models import Profile
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'signin.html')
 
 
 def signup(request):
@@ -14,7 +19,6 @@ def signup(request):
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
-
         if password == password2:
             if User.objects.filter(email=email).exists():
                 messages.info(request, 'Email taken')
@@ -25,12 +29,12 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('login')
         else:
             messages.info(request, 'Password not matching')
             return redirect('signup')
     else:
         return render(request, 'signup.html')
-
-
-def signin(request):
-    return render(request, 'signin')
