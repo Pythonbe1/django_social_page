ea7d30fbac23f9b9ca75a1ad4ab64420845bb89f
Pythonbe1 <bekbol.sultankulov2@gmail.com>
2022-10-17 00:44:20 +0600

    added login page

--- core/views.py
+++ core/views.py
@@ -12,7 +12,19 @@ def index(request):
 
 
 def signin(request):
-    return render(request, 'signin.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials invalid')
+            return redirect('/signin')
+
+    else:
+        return render(request, 'signin.html')
 
 
 def signup(request):
