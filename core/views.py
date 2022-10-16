30c11e7b5661569e91ab9d502d9a069dec932b1f
Pythonbe1 <bekbol.sultankulov2@gmail.com>
2022-10-16 23:55:59 +0600

    added sign up page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 
 def index(request):
@@ -13,9 +15,22 @@ def signup(request):
         password = request.POST['password']
         password2 = request.POST['password2']
 
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+        else:
+            messages.info(request, 'Password not matching')
+            return redirect('signup')
     else:
         return render(request, 'signup.html')
 
 
 def signin(request):
-    return render(request, 'signin.html')
+    return render(request, 'signin')
