2d5154e9185ffeaaf4d5a4e144f5ad3c05d44cc9
Pythonbe1 <bekbol.sultankulov2@gmail.com>
2022-10-17 01:02:22 +0600

    added login page

--- core/views.py
+++ core/views.py
@@ -5,8 +5,10 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -29,28 +31,40 @@ def signin(request):
 
 def signup(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        password2 = request.POST['password2']
-        if password == password2:
-            if User.objects.filter(email=email).exists():
-                messages.info(request, 'Email taken')
-                return redirect('signup')
-            elif User.objects.filter(username=username).exists():
-                messages.info(request, 'Username taken')
-                return redirect('signup')
-            else:
-                user = User.objects.create_user(username=username, email=email, password=password)
-                user.save()
-                user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
-                new_profile.save()
-                messages.info(request, 'Registered successfully')
-                time.sleep(2)
-                return redirect('signin')
+        try:
+
+            username = request.POST['username']
+            email = request.POST['email']
+            password = request.POST['password']
+            password2 = request.POST['password2']
+            if password == password2:
+                if User.objects.filter(email=email).exists():
+                    messages.info(request, 'Email taken')
+                    return redirect('signup')
+                elif User.objects.filter(username=username).exists():
+                    messages.info(request, 'Username taken')
+                    return redirect('signup')
+                else:
+                    user = User.objects.create_user(username=username, email=email, password=password)
+                    user.save()
+                    user_model = User.objects.get(username=username)
+                    new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                    new_profile.save()
+                    messages.info(request, 'Registered successfully')
+                    time.sleep(2)
+                    return redirect('signin')
+        except ValueError:
+            messages.info(request, 'Please enter username, email, and password')
+            return redirect('signup')
+
         else:
             messages.info(request, 'Password not matching')
             return redirect('signup')
+
     else:
         return render(request, 'signup.html')
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
